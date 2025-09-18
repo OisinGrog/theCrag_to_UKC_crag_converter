5f78b045078b800b371b18daa25c85d9530fadeb
OisinGrog <oisin130@gmail.com>
2025-09-18 12:28:14 +1000

    Using local test site for testing

--- tests/tests_theCrag_to_UKC_crag_converter.py
+++ tests/tests_theCrag_to_UKC_crag_converter.py
@@ -8,19 +8,19 @@ test_url = "https://www.thecrag.com/en/climbing/australia/hornsby-and-the-north/
 class TestRoutesCSV(unittest.TestCase):
 
     def test_get_fa(self):
-        routes = get_test_routes()
+        routes = get_local_test_routes()
         fa_info = converter.get_fa(routes[3])
         
-        self.assertEqual(fa_info.what, "FA")
+        self.assertEqual(fa_info.what, "FA:")
         self.assertEqual(fa_info.who, "Jack Schyvens & Daniel Butler")
         self.assertEqual(fa_info.when, "22 Jul 2017")
 
 if __name__ == "__main__":
     unittest.main()
 
-def get_test_routes():
+def get_local_test_routes():
     driver = converter.set_up_scrape()
-    driver.get(test_url)
-    time.sleep(5)  # Wait for the page to load completely
+    driver.get("file:///C:/Users/oisin/Coding%20Projects/theCrag_to_UKC_crag_converter/tests/test_site.html")
+    time.sleep(1)
     routes = driver.find_elements(By.CLASS_NAME, "route")
-    return routes
+    return routes   
