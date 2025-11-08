22d197742cb3260ba66e4f550fd12faee6b60b51
OisinGrog <oisin130@gmail.com>
2025-11-08 22:30:36 +1030

    Added test for scraper

--- tests/tests_theCrag_to_UKC_crag_converter.py
+++ tests/tests_theCrag_to_UKC_crag_converter.py
@@ -3,9 +3,23 @@ import theCrag_to_UKC_crag_converter as converter
 import time
 from selenium.webdriver.common.by import By
 
-test_url = "https://www.thecrag.com/en/climbing/australia/hornsby-and-the-north/area/1375085595"
+local_site = "file:///C:/Users/oisin/Coding%20Projects/theCrag_to_UKC_crag_converter/tests/test_site.html"
 
 class TestRoutesCSV(unittest.TestCase):
+
+    def test_scrape_route(self):
+        routes = converter.scrape_route(local_site)
+        test_route = routes[0]
+
+        self.assertEqual(test_route.name, "Hidey-Hole")
+        self.assertEqual(test_route.climb_type, "Boulder")
+        self.assertEqual(test_route.grade, "V0")
+        self.assertEqual(test_route.stars, 1)
+        self.assertEqual(test_route.height, "6m")
+        self.assertEqual(test_route.description, "Starting from the left hand side, climb almost diagonal towards the hole near the top. (Where a nice no hands rest is available). From here its just a matter of standing up to reach a jug and then over the top.")
+        self.assertIsNone(test_route.bolts)
+        self.assertIsNotNone(test_route.fa)
+        self.assertIsNone(test_route.pitches)
 
     def test_get_fa(self):
         routes = get_local_test_routes()
